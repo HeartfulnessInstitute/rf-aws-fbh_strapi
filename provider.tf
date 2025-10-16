bd8ae04a5bc008b67fbdaadb0944931bc042478e
shwetha-4371 <shwetha.s@volunteer.heartfulness.org>
2025-10-16 18:32:16 +0530

    changes updated provider.tf

--- provider.tf
+++ provider.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_version = ">= 1.5.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 # Default provider (used by resources that reference `aws`)
 provider "aws" {
   region = "ap-south-1"
